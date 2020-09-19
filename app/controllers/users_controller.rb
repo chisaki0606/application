a9e57d143557d45cf2f16d3066ae6ceb939dc9a2
“chisaki0606” <witness6961@gmail.com>
2020-09-19 22:13:20 +0900

    add user details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController < ApplicationController
 
   def form
   end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/" => "users#list"
-  get "users/form" => "users#form" 
+  get "users/form" => "users#form"
+  get "users/:id" => "users#show" 
 end
