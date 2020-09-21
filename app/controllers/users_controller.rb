6002119731caf6cc7e244929213e4003752cc1ad
“chisaki0606” <witness6961@gmail.com>
2020-09-21 11:49:42 +0900

    add read

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   def list
-    @users = User.all
+    @users = User.all.order(created_at: :desc)
   end
 
   def form
@@ -15,6 +15,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(name: params[:name])
+    @user.save
+    redirect_to("/")
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
     @user.save
     redirect_to("/")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   get "/" => "users#list"
   get "users/form" => "users#form"
-  get "users/:id" => "users#show" 
+  get "users/:id" => "users#show"
+  get "users/:id/edit" => "users#edit"
   get "users/new" => "user#new"
   post "users/create" => "users#create"
+  post "users/:id/update" => "users#update"
 end
