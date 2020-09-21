5ac509ee3fcddbdb9fd1918e016c5a3dab972bd6
“chisaki0606” <witness6961@gmail.com>
2020-09-21 10:35:38 +0900

    add create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,4 +9,13 @@ class UsersController < ApplicationController
   def show
     @user = User.find_by(id: params[:id])
   end
+
+  def new
+  end
+
+  def create
+    @user = User.new(name: params[:name])
+    @user.save
+    redirect_to("/")
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get "/" => "users#list"
   get "users/form" => "users#form"
   get "users/:id" => "users#show" 
+  get "users/new" => "user#new"
+  post "users/create" => "users#create"
 end
